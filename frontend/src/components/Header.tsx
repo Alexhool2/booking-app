f92090e7c4c1da881ad96b88e151d668b6d31837
Alexhool2 <alederbal@hotmail.com>
2024-06-17 04:12:21 +0900

    git

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import { useAppContext } from '../contexts/AppContext'
 const Header = () => {
+  const { isLoggedIn } = useAppContext()
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
@@ -7,12 +9,29 @@ const Header = () => {
           <Link to="/">MernHolidays.com</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="bg-white flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link
+                to="/my-bookings"
+                className="text-white hover:text-gray-300"
+              >
+                My bookings
+              </Link>
+              <Link to="/my-hotels" className="text-white hover:text-gray-300">
+                My Hotels
+              </Link>
+              <button className="text-white hover:text-gray-300">
+                Sign out
+              </button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="bg-white flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
         </span>
       </div>
     </div>
