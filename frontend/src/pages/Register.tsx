85f96e7b0be610e441b8e23d4e25416835cfb934
Alexhool2 <alederbal@hotmail.com>
2024-06-17 03:20:22 +0900

    a

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
 import * as apiClient from '../api-client'
 import { useAppContext } from '../contexts/AppContext'
+import { useNavigate } from 'react-router-dom'
 export type RegisterFormData = {
   firstName: string
   lastName: string
@@ -11,6 +12,7 @@ export type RegisterFormData = {
 }
 
 export const Register = () => {
+  const navigate = useNavigate()
   const { showToast } = useAppContext()
   const {
     register,
@@ -22,6 +24,7 @@ export const Register = () => {
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
       showToast({ message: 'Successfully registered!', type: 'SUCCESS' })
+      navigate('/')
     },
     onError: (error: Error) => {
       showToast({ message: error.message, type: 'ERROR' })
